289c31056294308462a4444069f06905c9c650a6
Seun Olaifa <seunolaifa19@yahoo.com>
2019-07-11 00:50:42 +0100

    Second commit

--- test/index.spec.js
+++ test/index.spec.js
@@ -7,6 +7,6 @@ const expect = require('chai').expect;
 
 describe('test', () => {
     it('should return a string', () => {
-        expect('ci with travis').to.equal('ci with travis');
+        expect('CI with Travis').to.equal('CI with Travis');
     });
 });
